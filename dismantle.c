d9e2e0a390d8815f6fe225a6827ceb20f0c84ee6
vext01 <vext01@gmail.com>
2011-07-16 12:08:12 +0100

    search for a section

--- dismantle.c
+++ dismantle.c
@@ -30,6 +30,72 @@ dm_disasm_op(ud_t *ud, FILE *f, int addr)
 	return (addr + read);
 }
 
+/*
+ * get the offset of a section name
+ */
+long
+dm_find_section(FILE *f, char *find_sec)
+{
+	Elf			*elf;
+	Elf_Kind		 ek;
+	Elf_Scn			*sec;
+	size_t			 shdrs_idx, i;
+	GElf_Shdr		 shdr;
+	char			*sec_name;
+	long			 ret = -1;
+
+	if(elf_version(EV_CURRENT) == EV_NONE) {
+		fprintf(stderr, "elf_version: %s\n", elf_errmsg(-1));
+		goto clean;
+	}
+
+	if ((elf = elf_begin(fileno(f), ELF_C_READ, NULL)) == NULL) {
+		fprintf(stderr, "elf_begin: %s\n", elf_errmsg(-1));
+		goto clean;
+	}
+
+	ek = elf_kind(elf);
+
+	if (ek != ELF_K_ELF) {
+		fprintf(stderr, "Does not appear to have an ELF header\n");
+		goto clean;
+	}
+
+	if (elf_getshdrstrndx(elf, &shdrs_idx) != 0) {
+		fprintf(stderr, "elf_getshdrsrtndx: %s", elf_errmsg(-1));
+		goto clean;
+	}
+
+	sec = NULL ;
+	while ((sec = elf_nextscn(elf, sec)) != NULL) {
+		if (gelf_getshdr(sec, &shdr) != &shdr) {
+			fprintf(stderr, "gelf_getshdr: %s", elf_errmsg(-1));
+			goto clean;
+		}
+
+		if ((sec_name =
+		    elf_strptr(elf, shdrs_idx, shdr.sh_name)) == NULL) {
+			fprintf(stderr, "elf_strptr: %s", elf_errmsg(-1));
+			goto clean;
+		}
+
+		if (strcmp(sec_name, find_sec) == 0) {
+			ret = shdr.sh_offset;
+			break;
+		}
+
+		printf("0x%08x: %s\n", (long) shdr.sh_offset, sec_name);
+	}
+
+	printf("\n");
+
+clean:
+	elf_end(elf);
+	return (ret);
+}
+
+
+
 void
 dm_dump_elf_info(FILE *f)
 {
@@ -111,8 +177,9 @@ int
 main(int argc, char **argv)
 {
 	ud_t			 ud;
-	int			 i, addr = 0x00000160, ops = 8;
+	int			 i, ops = 8;
 	FILE			*f;
+	long			addr;
 
 	if (argc == 3) {
 		addr = atoi(argv[1]);
@@ -130,6 +197,9 @@ main(int argc, char **argv)
 	ud_set_input_file(&ud, f);
 	ud_set_mode(&ud, 64);
 	ud_set_syntax(&ud, UD_SYN_INTEL);
+
+	addr = dm_find_section(f, ".text");
+	printf("Seeking to .text at %08x\n", addr);
 
 	for (i = 0; i < ops; i++)
 		addr = dm_disasm_op(&ud, f, addr);
