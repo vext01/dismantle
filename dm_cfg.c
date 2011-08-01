c611b825a4fcc5973ea61f3b1c8606f77c3f1cd6
vext01 <vext01@gmail.com>
2011-08-01 12:01:58 +0100

    code style

--- dm_cfg.c
+++ dm_cfg.c
@@ -1,47 +1,71 @@
+/*
+ * Copyright (c) 2011, Ed Robbins <edd.robbins@gmail.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <stdio.h>
 #include "dm_cfg.h"
 
-/* A linked list to keep track of all the CFG blocks so we can free them at the end */
+/*
+ * A linked list to keep track of all the CFG
+ * blocks so we can free them at the end.
+ * XXX queue.h
+ */
 struct ptrs {
-	void* ptr;
-	struct ptrs *next;
+	void		*ptr;
+	struct ptrs	*next;
 };
+
 /* Head of the list and a list iterator */
-struct ptrs *p_head = NULL;
-struct ptrs *p = NULL;
+struct ptrs		*p_head = NULL;
+struct ptrs		*p = NULL;
 
 /*
- * Generate static CFG for a function (continues until it reaches ret), does not follow calls
+ * Generate static CFG for a function.
+ * Continues until it reaches ret, does not follow calls.
  */
 int
-dm_cmd_cfg(char **args) {
+dm_cmd_cfg(char **args)
+{
 	NADDR		addr = cur_addr;
-	struct		dm_instruction_se *instructions = NULL;	
+	struct		dm_instruction_se *instructions = NULL;
 	struct		dm_cfg_node *cfg = NULL;
-	
+
 	(void) args;
 
 	dm_instruction_se_init(&instructions);
-	
+
 	/* Initialise free list */
 	p_head = calloc(1, sizeof(struct ptrs));
 	p = p_head;
-	
+
 	/* Create first node */
 	cfg = dm_new_cfg_node(addr, 0);
-	
-	/* Create CFG */	
+
+	/* Create CFG */
 	dm_gen_cfg_block(cfg, instructions);
 
 	/* Print CFG */
 	dm_print_cfg(cfg);
-	
+
 	/* Free all memory */
 	dm_cfg_free();
 	free(instructions);
 
 	/* Rewind back */
 	dm_seek(addr);
+
 	return (0);
 }
 
@@ -51,39 +75,41 @@ void
 dm_instruction_se_init(struct dm_instruction_se **instructions) {
 	int i;
 
-	*instructions = malloc(sizeof(struct dm_instruction_se) * (DM_UD_ENUM_HACK));
-	
+	*instructions =
+	    malloc(sizeof(struct dm_instruction_se) * (DM_UD_ENUM_HACK));
+
 	/* Initialise struct recording which instructions write to registers */
 	for (i = 0; i < DM_UD_ENUM_HACK; i++) {
-		(*instructions)[i].instruction = i;//(enum ud_mnemonic_code)i;
+		(*instructions)[i].instruction = i;
 		(*instructions)[i].write = 1;
 		(*instructions)[i].jump = 0;
 		(*instructions)[i].ret = 0;
 	}
-	
+
+	/* XXX store in linked list (queue.h) */
 	(*instructions)[UD_Ipush].write = 0;
 	(*instructions)[UD_Itest].write = 0;
 
 	(*instructions)[UD_Iret].write = 0;
 	(*instructions)[UD_Iret].ret = 1;
-	
+
 	(*instructions)[UD_Ijmp].write = 0;
 	(*instructions)[UD_Ijmp].jump = 1;
-	
+
 	(*instructions)[UD_Ijz].write = 0;
 	(*instructions)[UD_Ijz].jump = 2;
-	
+
 	(*instructions)[UD_Ijnz].write = 0;
 	(*instructions)[UD_Ijnz].jump = 2;
-	
+
 	(*instructions)[UD_Icmp].write = 0;
-	
+
 	(*instructions)[UD_Ijg].write = 0;
 	(*instructions)[UD_Ijg].jump = 2;
-	
+
 	(*instructions)[UD_Ijle].write = 0;
 	(*instructions)[UD_Ijle].jump = 2;
-	
+
 	(*instructions)[UD_Icall].write = 0;
 	(*instructions)[UD_Icall].jump = 0;
 }
@@ -91,25 +117,30 @@ dm_instruction_se_init(struct dm_instruction_se **instructions) {
 /*
  * Create a new node in the CFG
  */
-struct dm_cfg_node* 
-dm_new_cfg_node(NADDR nstart, NADDR nend) {
-	struct dm_cfg_node* node;
+struct dm_cfg_node *
+dm_new_cfg_node(NADDR nstart, NADDR nend)
+{
+	struct dm_cfg_node		*node;
+
 	node = malloc(sizeof(struct dm_cfg_node));
 	node->start = nstart;
 	node->end = nend;
 	node->children = calloc(1, sizeof(void*));
-	/* Add this node to the free list so we can free the memory at the end */
+
+	/* Add node to the free list so we can free the memory at the end */
 	p->ptr = (void*)node;
 	p->next = calloc(1, sizeof(struct ptrs));
 	p = p->next;
-	return node;
+
+	return (node);
 }
 
-struct dm_cfg_node* 
+struct dm_cfg_node *
 dm_gen_cfg_block(struct dm_cfg_node *node, struct dm_instruction_se *instructions) {
-	NADDR 			addr = node->start;
-	unsigned int 		read = 0, oldRead = 0;
-	char 			*hex;
+
+	NADDR			addr = node->start;
+	unsigned int		read = 0, oldRead = 0;
+	char			*hex;
 	struct dm_cfg_node	*foundNode = NULL;
 
 	dm_seek(node->start);
@@ -117,44 +148,75 @@ dm_gen_cfg_block(struct dm_cfg_node *node, struct dm_instruction_se *instruction
 		oldRead = read;
 		read = ud_disassemble(&ud);
 		hex = ud_insn_hex(&ud);
-		
+
 		/* Check we haven't run into the start of another block */
-		if ((foundNode = dm_find_cfg_node_starting(addr)) && (foundNode != node)) {
+		if ((foundNode = dm_find_cfg_node_starting(addr))
+		    && (foundNode != node)) {
 			addr -= oldRead;
 			free(node->children);
 			node->children = calloc(2, sizeof(void*));
 			node->children[0] = foundNode;
 			break;
 		}
-		
-		/* Check for jump instructions and create new nodes as necessary */
+
+		/*
+		 * Check for jump instructions and create
+		 * new nodes as necessary
+		 */
 		if (instructions[ud.mnemonic].jump) {
 			/* End the block here */
 			node->end = addr;
 			free(node->children);
-			/* Make space for the children of this block */	
-			node->children = calloc(instructions[ud.mnemonic].jump + 1, sizeof(void*));	
-			
-			/* Check if we are jumping to the start of an already existing block, if so use that as child of current block */
-			if ((node->children[0] = dm_find_cfg_node_starting(ud.operand[0].lval.sdword + addr + read)) == NULL)
-				/* Check if we are jumping to the middle of an already existing block, 
-				 * if so split it and use 2nd half as child of current block */
-				if ((node->children[0] = dm_find_cfg_node_containing(ud.operand[0].lval.sdword + addr + read)) == NULL) {
-					/* This is a new block, so scan do a recursive call to find it's start, end, and children */
-					node->children[0] = dm_new_cfg_node(ud.operand[0].lval.sdword + addr + read, 0);
-					dm_gen_cfg_block(node->children[0], instructions);
+
+			/* Make space for the children of this block */
+			node->children = calloc(instructions[ud.mnemonic].jump
+			    + 1, sizeof(void*));
+
+			/*
+			 * Check if we are jumping to the start of an already
+			 * existing block, if so use that as child of current
+			 * block
+			 */
+			if ((node->children[0] = dm_find_cfg_node_starting(
+			    ud.operand[0].lval.sdword + addr + read)) == NULL)
+				/*
+				 * Check if we are jumping to the *middle* of
+				 * an already existing block, if so split it
+				 * and use 2nd half as child of current block
+				 */
+				/* XXX nesting level too deep */
+				if ((node->children[0] =
+				    dm_find_cfg_node_containing(
+				    ud.operand[0].lval.sdword + addr + read))
+				    == NULL) {
+					/* This is a new block, so scan do a
+					 * recursive call to find it's start,
+					 * end, and children
+					 */
+					/* XXX nesting level too deep */
+					node->children[0] = dm_new_cfg_node(
+					    ud.operand[0].lval.sdword +
+					    addr + read, 0);
+					dm_gen_cfg_block(
+					    node->children[0], instructions);
 				}
-			
-			/* Seek back to where we were before we followed the jump */
+
+			/* Seek back to before we followed the jump */
 			dm_seek(addr);
 			read = ud_disassemble(&ud);
-			
-			/* If the jump was a conditional, now we must follow the other leg of the jump */
+
+			/*
+			 * If the jump was a conditional, now we must
+			 * follow the other leg of the jump
+			 */
 			if (instructions[ud.mnemonic].jump > 1) {
-				if ((node->children[1] = dm_find_cfg_node_starting(addr + read)) != NULL)
+				if ((node->children[1] =
+				    dm_find_cfg_node_starting(addr + read))
+				    != NULL)
 					break;
 				else {
-					node->children[1] = dm_new_cfg_node(addr + read, 0);
+					node->children[1] =
+					    dm_new_cfg_node(addr + read, 0);
 					node = node->children[1];
 				}
 			}
@@ -170,13 +232,16 @@ dm_gen_cfg_block(struct dm_cfg_node *node, struct dm_instruction_se *instruction
 	return node;
 }
 
-/* 
- * Searches all blocks for one starting with addr, and returns it if found 
- * (otherwise returns NULL) 
+/*
+ * Searches all blocks for one starting with addr, and returns it if found
+ * (otherwise returns NULL)
  */
-struct dm_cfg_node*
-dm_find_cfg_node_starting(NADDR addr) {
-	struct dm_cfg_node *node;
+struct dm_cfg_node *
+dm_find_cfg_node_starting(NADDR addr)
+{
+
+	struct dm_cfg_node		*node;
+
 	p = p_head;
 	while (p->ptr != NULL) {
 		node = (struct dm_cfg_node*)(p->ptr);
@@ -184,66 +249,92 @@ dm_find_cfg_node_starting(NADDR addr) {
 			return node;
 		p = p->next;
 	}
-	return NULL;
+
+	return (NULL);
 }
 
-/* 
- * Searches all blocks to see if one contains addr. If so, split the block and return the tail 
- * (otherwise returns NULL)
+/*
+ * Searches all blocks to see if one contains addr. If so,
+ * split the block and return the tail (otherwise returns NULL)
  */
-struct dm_cfg_node*
-dm_find_cfg_node_containing(NADDR addr) {
-	struct dm_cfg_node *node, *newNode;
-	NADDR addr2;
-	unsigned int read = 0;
+struct dm_cfg_node *
+dm_find_cfg_node_containing(NADDR addr)
+{
+	struct dm_cfg_node		*node, *newNode;
+	NADDR				addr2;
+	unsigned int			read = 0;
+
 	p = p_head;
 	while (p->ptr != NULL) {
-		node = (struct dm_cfg_node*)(p->ptr);
-		if ((node->start > addr) && (node->end != 0) && (node->end < addr)) {
-			/* We found a matching block
-			 * Now find address before addr and splot the block, return tail block */
-			for (dm_seek(node->start); addr2 + read != addr; addr2 += read)
+
+		node = (struct dm_cfg_node*) (p->ptr);
+
+		if ((node->start > addr) &&
+		    (node->end != 0) && (node->end < addr)) {
+
+			/*
+			 * We found a matching block. Now find address before
+			 * addr and splot the block, return tail block
+			 */
+			for (dm_seek(node->start);
+			    addr2 + read != addr; addr2 += read)
 				read = ud_disassemble(&ud);
+
 			newNode = dm_new_cfg_node(addr, node->end);
 			newNode->children = node->children;
+
 			node->children = calloc(2, sizeof(void*));
 			node->children[0] = newNode;
 			node->end = addr2;
-			return newNode;
+
+			return (newNode);
 		}
+
 		p = p->next;
 	}
-	return NULL;
+	return (NULL);
 }
 
 /*
  * Use the free list to print info on all the blocks we have found
  */
-void dm_print_cfg() {
-	struct dm_cfg_node* node;
-	int i;
+void
+dm_print_cfg()
+{
+
+	struct dm_cfg_node		*node;
+	int				i;
+
 	p = p_head;
 	while (p->ptr != NULL) {
-		node = (struct dm_cfg_node*)(p->ptr);
-		printf("Block start: " NADDR_FMT ", end: " NADDR_FMT "\n", 
-		    node->start, node->end);
-		for (i = 0; node->children[i] != NULL; i++)
-			printf("\tChild %d start: " NADDR_FMT ", end: " NADDR_FMT "\n", i, 
-			    node->children[i]->start, 
+		node = (struct dm_cfg_node*) (p->ptr);
+
+		printf("Block start: " NADDR_FMT ", end: "
+		    NADDR_FMT "\n", node->start, node->end);
+
+		for (i = 0; node->children[i] != NULL; i++) {
+			printf("\tChild %d start: " NADDR_FMT ", end: "
+			    NADDR_FMT "\n", i, node->children[i]->start,
 			    node->children[i]->end);
+		}
 		p = p->next;
 	}
 }
 
-/* 
+/*
  * Free all data structures used for building the CFG
  */
-void 
-dm_cfg_free() {
-	struct ptrs* p_prev;
+void
+dm_cfg_free()
+{
+	struct ptrs		*p_prev;
+
 	p = p_head;
 	while (p != NULL) {
-		if (p->ptr != NULL) free(((struct dm_cfg_node*)(p->ptr))->children);
+
+		if (p->ptr != NULL)
+			free(((struct dm_cfg_node*)(p->ptr))->children);
+
 		free(p->ptr);
 		p_prev = p;
 		p = p->next;
@@ -251,48 +342,49 @@ dm_cfg_free() {
 	}
 }
 
-int 
-dm_cmd_ssa(char **args) {
-	struct 		dm_ssa_index  *indices = NULL;
+int
+dm_cmd_ssa(char **args)
+{
+	struct		dm_ssa_index  *indices = NULL;
 	struct		dm_instruction_se *instructions = NULL;
 	unsigned int	read;
 	char		*hex;
 	int		ops = strtoll(args[0], NULL, 0), i;
 	NADDR		addr = cur_addr;
-	
+
 	dm_ssa_index_init(&indices);
 	dm_instruction_se_init(&instructions);
-	
+
 	printf("\n");
 	for (i = 0; i < ops; i++) {
 		read = ud_disassemble(&ud);
 		hex = ud_insn_hex(&ud);
+
 		printf("    " NADDR_FMT ":  %-20s%s  ",
 		    addr, hex, ud_insn_asm(&ud));
+
 		addr = addr + read;
+
 		if (instructions[ud.mnemonic].write)
 			indices[ud.operand[0].base].index++;
-		/*sprintf(out, "%s", ud_insn_asm(&ud));
-		if ((srch = strchr(out, ',')) != NULL)
-		printf(*/
-			
+
 		printf("%d, ", indices[ud.operand[0].base].index);
 		printf("%d, ", indices[ud.operand[1].base].index);
-		//	if (ud.operand[2] != NULL)
 		printf("%d\n", indices[ud.operand[2].base].index);
-		//}		
 	}
-	
+
 	dm_seek(cur_addr); /* rewind back */
 	printf("\n");
 	return (0);
 }
 
-void 
-dm_ssa_index_init(struct dm_ssa_index **indices) {
-	int i;
+void
+dm_ssa_index_init(struct dm_ssa_index **indices)
+{
+	int			i;
 
 	*indices = malloc(sizeof(struct dm_ssa_index) * (UD_OP_CONST + 1));
+
 	/* Initialise struct for SSA indexes */
 	for (i = 0; i < UD_OP_CONST + 1; i++) {
 		(*indices)[i].reg = (enum ud_type)i;
--- dm_cfg.h
+++ dm_cfg.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2011, Ed Robbins <edd.robbins@gmail.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #ifndef __CFG_H
 #define __CFG_H
 
@@ -5,7 +21,7 @@
 #include "dm_dis.h"
 
 struct dm_ssa_index {
-	enum ud_type 	reg;
+	enum ud_type			reg;
 	int				index;
 };
 
@@ -22,18 +38,19 @@ struct dm_cfg_node {
 	struct dm_cfg_node **children;
 };
 
-void 			dm_ssa_index_init(struct dm_ssa_index **indices);
-void 			dm_instruction_se_init(struct dm_instruction_se **instructions);
+void		dm_ssa_index_init(struct dm_ssa_index **indices);
+void		dm_instruction_se_init(struct dm_instruction_se **instrs);
 
-int 			dm_cmd_ssa(char **args);
-int 			dm_cmd_cfg(char **args);
+int		dm_cmd_ssa(char **args);
+int		dm_cmd_cfg(char **args);
 
-struct dm_cfg_node* 	dm_new_cfg_node(NADDR nstart, NADDR nend);
-void 			dm_print_cfg();
-void 			dm_cfg_free();
-struct dm_cfg_node* 	dm_gen_cfg_block(struct dm_cfg_node *node, struct dm_instruction_se *instructions);
+struct dm_cfg_node	*dm_new_cfg_node(NADDR nstart, NADDR nend);
+void			 dm_print_cfg();
+void			 dm_cfg_free();
+struct dm_cfg_node	*dm_gen_cfg_block(struct dm_cfg_node *node,
+			     struct dm_instruction_se *instructions);
 
-struct dm_cfg_node*  	dm_find_cfg_node_starting(NADDR addr);
-struct dm_cfg_node* 	dm_find_cfg_node_containing(NADDR addr);
+struct dm_cfg_node	*dm_find_cfg_node_starting(NADDR addr);
+struct dm_cfg_node	*dm_find_cfg_node_containing(NADDR addr);
 
 #endif
