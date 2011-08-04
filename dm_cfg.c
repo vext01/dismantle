41ccd1f87fea83be6ab7a5589b6aa325c33291fd
static-void <edd.robbins@gmail.com>
2011-08-04 11:30:50 +0100

    Fixed some warnings and squished one small bug

--- dm_cfg.c
+++ dm_cfg.c
@@ -191,11 +191,10 @@ dm_add_parent(struct dm_cfg_node *node, struct dm_cfg_node *parent)
 struct dm_cfg_node *
 dm_gen_cfg_block(struct dm_cfg_node *node)
 {
-	NADDR			addr = node->start, addr2 = 0;
+	NADDR			addr = node->start;
 	unsigned int		read = 0, oldRead = 0;
 	char			*hex;
-	struct dm_cfg_node	*foundNode = NULL, *newNode = NULL;
-	int i = 0, j = 0;
+	struct dm_cfg_node	*foundNode = NULL;
 
 	dm_seek(node->start);
 	while (1) {
@@ -265,7 +264,6 @@ dm_gen_cfg_block(struct dm_cfg_node *node)
 			/* Check whether there was some sneaky splitting of the
 			 * block we're working on while we were away! */
 			if (node->end < addr) {
-				printf("Node was split while we were away! Eeek!\n");
 				/* Now we must find the right block to continue
 				 * from */
 				foundNode = dm_find_cfg_node_ending(addr);
@@ -373,9 +371,8 @@ dm_find_cfg_node_ending(NADDR addr)
 
 	for (p_iter = p_head; p_iter->ptr != NULL; p_iter = p_iter->next) {
 		node = (struct dm_cfg_node*)(p_iter->ptr);
-		if (node->end == addr) {
-			printf("Found node ending with addr!\n");
-		}
+		if (node->end == addr)
+			return (node);
 	}
 	return (NULL);
 }
